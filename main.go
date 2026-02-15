79a621999825b0e6f27c67049bdd21bda068d8ce
crushr3sist <silent.death3500@gmail.com>
2026-02-16 08:04:52 +1000

    fiber initalised for frontend serving

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func main() {
-	fmt.Println("hello world")
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+	fmt.Println("listening on http://localhost:3000/")
+
+	app.Listen(":3000")
+
 }
