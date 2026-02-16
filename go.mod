2c0bdac857db75ce15ed20fea094e2c53b37806d
crushr3sist <silent.death3500@gmail.com>
2026-02-16 10:03:54 +1000

    serving static and html files while embedded

--- go.mod
+++ go.mod
@@ -2,16 +2,22 @@ module SOLACE.sys
 
 go 1.25.0
 
+require github.com/gofiber/template/html/v3 v3.0.2
+
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/gofiber/fiber/v2 v2.52.11 // indirect
 	github.com/gofiber/fiber/v3 v3.0.0 // indirect
 	github.com/gofiber/schema v1.6.0 // indirect
+	github.com/gofiber/template/v2 v2.1.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.18.3 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/tinylib/msgp v1.6.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.69.0 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/static"
+	"github.com/gofiber/template/html/v3"
 )
 
-func main() {
-	app := fiber.New()
+//go:embed frontend/*
+var frontend_dir embed.FS
 
+//go:embed frontend/static/*
+var static_dir embed.FS
+
+func main() {
+
+	// create a views engine
+	engine := html.NewFileSystem(http.FS(frontend_dir), ".html")
+
+	// start a new fiber instance
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	// embed the static files
+	static_files := fs.FS(static_dir)
+
+	// use those static files
+	app.Use("/", static.New("", static.Config{FS: static_files}))
+
+	// serve the index.html page
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello World")
+		return c.Render("frontend/index", fiber.Map{
+			"Title": "Hello, World!",
+		})
 	})
 
 	fmt.Println("listening on http://localhost:3000/")
 
-	app.Listen(":3000")
+	// start the server
+	log.Fatal(app.Listen(":3000"))
 
 }
